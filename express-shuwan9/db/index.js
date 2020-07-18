2e2b0268a37a4d6645e89b475297244ed3dbbd50
shu-wan9 <18762750392@163.com>
2020-07-18 17:34:00 +0800

    add temp

--- express-shuwan9/db/index.js
+++ express-shuwan9/db/index.js
@@ -3,7 +3,7 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
 // const adapter = new FileSync(path.join(__dirname, "./db.json"));
-const adapter = new FileSync(path.join(__dirname, "../../tmp/db.json"));
+const adapter = new FileSync(path.join(__dirname, "../temp/db.json"));
 const db = low(adapter);
 
 module.exports = db;
