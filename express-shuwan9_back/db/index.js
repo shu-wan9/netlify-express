8db0be4b89e384bc53076d349268596334ff46a6
shu-wan9 <18762750392@163.com>
2020-07-18 19:46:08 +0800

    use path.resolve

--- express-shuwan9_back/db/index.js
+++ express-shuwan9_back/db/index.js
@@ -5,7 +5,8 @@ const FileSync = require("lowdb/adapters/FileSync");
 // const adapter = new FileSync(path.join(__dirname, "../temp/db.json"));
 // const adapter = new FileSync(path.resolve("temp/db.json"));
 // const adapter = new FileSync(path.resolve("db.json"));
-const adapter = new FileSync("db.json");
+// const adapter = new FileSync("db.json");
+const adapter = new FileSync(require.resolve("./db.json"));
 const db = low(adapter);
 
 module.exports = db;
