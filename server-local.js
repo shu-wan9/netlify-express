a478839a699798c84f608686470839004db47739
shu-wan9 <18762750392@163.com>
2020-07-18 18:28:11 +0800

    update

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const app = require("./express-shuwan9/server");
+const app = require("./express-shuwan9_back/server");
 
 app.listen(3000, () => console.log("Local app listening on port 3000!"));
