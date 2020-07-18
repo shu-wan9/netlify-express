c14c14b95bf45c0076310d919a5a3310ff2abc76
shu-wan9 <18762750392@163.com>
2020-07-18 20:18:04 +0800

    use proxy

--- server-local.js
+++ server-local.js
@@ -1,5 +1,5 @@
 "use strict";
 
-const app = require("./express-shuwan9_back/server");
+const app = require("./strong-body/server");
 
 app.listen(3000, () => console.log("Local app listening on port 3000!"));
---
+++ strong-body/server.js
@@ -0,0 +1,79 @@
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+const app = express();
+const bodyParser = require("body-parser");
+const got = require("got");
+
+const TARGET = "http://180.76.114.135:8080";
+const GET_URL = "/api/exercise/getState";
+const POST_URL = "/api/exercise/setState";
+
+async function getState() {
+  return got.get(TARGET + GET_URL).then((res) => {
+    return res.body;
+  });
+}
+async function setState(exerciseData) {
+  const options = {
+    json: {
+      exerciseData,
+    },
+    responseType: "json",
+  };
+  return got.post(TARGET + POST_URL, options).then((res) => res.body);
+}
+
+const router = express.Router();
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
+  res.end();
+});
+router.get(GET_URL, async (req, res) => {
+  try {
+    const data = await getState();
+    res.sendData(data, "查询成功");
+  } catch (e) {
+    console.log(e);
+    res.sendMsg("查询失败");
+  }
+});
+router.post(POST_URL, async (req, res) => {
+  try {
+    const { exerciseData } = req.body;
+    await setState({
+      exerciseData,
+    });
+    res.sendMsg("成功");
+  } catch (e) {
+    console.log(e);
+    res.sendMsg("失败");
+  }
+});
+
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.sendMsg = function (msg) {
+    res.set("Content-Type", "application/json;chartset=utf-8");
+    res.json({
+      code: 100,
+      msg,
+    });
+  };
+  res.sendData = function (data, msg = "查询成功") {
+    res.set("Content-Type", "application/json;chartset=utf-8");
+    res.json({
+      code: 100,
+      msg,
+      data,
+    });
+  };
+  next();
+});
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+
+module.exports = app;
+module.exports.handler = serverless(app);
