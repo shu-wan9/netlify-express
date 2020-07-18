f8250a6e04a5c5aa23d12761a1f32242a163e5e0
shu-wan9 <18762750392@163.com>
2020-07-18 17:11:18 +0800

    update

--- express-shuwan9/db/index.js
+++ express-shuwan9/db/index.js
@@ -2,7 +2,8 @@ const path = require("path");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync(path.join(__dirname, "../../tmp/db.json"));
+const adapter = new FileSync(path.join(__dirname, "./db.json"));
+// const adapter = new FileSync(path.join(__dirname, "../../tmp/db.json"));
 const db = low(adapter);
 
 module.exports = db;
